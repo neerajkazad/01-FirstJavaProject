4ccf48e092d6c9400772c806b6c13bc3b2784104
Neeaj Kumar <neeraj170291.kumar@gmail.com>
2017-06-29 17:37:20 +0530

    Second commit !

--- src/com/yin/first/HelloWorld.java
+++ src/com/yin/first/HelloWorld.java
@@ -3,6 +3,8 @@ package com.yin.first;
 public class HelloWorld {
 	public static void main(String[] args) {
 		//sample message
+		/// this is my second commit !
+		/// this will be shown only after i commit !
 		System.out.println("Hi Ubuntu OS");
 	}
 }
